5677f9ddda0a6ba6dc195a16e2e15887f2040001
Ziyuan Chen <ziyuanc3@illinois.edu>
2023-02-09 17:48:55 -0600

    Draft version HTTP client

--- mp1/src/http_client.c
+++ mp1/src/http_client.c
@@ -14,8 +14,11 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to
-#define MAXDATASIZE 100 // max number of bytes we can get at once
+#define MAX_HOSTNAME_LEN 256
+#define MAX_PORT_LEN 8
+#define MAX_FILENAME_LEN 256
+#define MAX_REQUEST_LEN 512
+#define MAX_RESPONSE_LEN 1048576
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -31,13 +34,38 @@ int main(int argc, char *argv[])
 {
 	int sockfd, rv, numbytes;
 	char addr[INET6_ADDRSTRLEN];
-	char buf[MAXDATASIZE];
 	struct addrinfo hints, *servinfo, *p;
 
 	if (argc != 2) {
 		fprintf(stderr, "usage: client hostname\n");
-		exit(1);
+		return 1;
 	}
+
+	/* analyze input argument */
+
+	char hostname[MAX_HOSTNAME_LEN];
+	char port[MAX_PORT_LEN];
+	char filename[MAX_FILENAME_LEN];
+	char *arg = argv[1];
+
+	if strncmp(arg, "http://", 7) != 0 {
+		fprintf(stderr, "Not a valid URL\n");
+		return 1;
+	} else {
+		arg = &arg[7];
+	}
+
+	int colon_pos = strcspn(arg, ":");
+	strncpy(hostname, arg, colon_pos);
+	hostname[colon_pos] = '\0';
+	arg = &arg[colon_pos + 1];
+
+	int slash_pos = strcspn(arg, "/");
+	strncpy(port, arg, slash_pos);
+	port[slash_pos] = '\0';
+	arg = &arg[slash_pos + 1];
+
+	strcpy(filename, arg);
 
 	/* bind to socket */
 
@@ -45,7 +73,7 @@ int main(int argc, char *argv[])
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -75,13 +103,36 @@ int main(int argc, char *argv[])
 	printf("client: connecting to %s\n", addr);
 	freeaddrinfo(servinfo); // all done with this structure
 
-	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	// send request
+	char request[MAX_REQUEST_LEN];
+	sprintf(request, "GET /%s HTTP/1.1\r\n\r\n", filename);
+	printf("client: sending '%s'\n", request);
+	if (send(sockfd, request, strlen(request), 0) == -1) {
+		perror("send");
+		return 1;
+	}
+
+	char response[MAX_RESPONSE_LEN];
+	if ((numbytes = recv(sockfd, response, MAX_RESPONSE_LEN-1, 0)) == -1) {
 		perror("recv");
-		exit(1);
+		return 1;
 	}
 	buf[numbytes] = '\0';
 
-	printf("client: received '%s'\n", buf);
+	// parse response
+	int content_ptr = strcspn(response, "\r\n\r\n");
+	response[content_ptr] = '\0';
+	printf("client: received '%s'\n", response);
+	char *content = &response[content_ptr + 4];
+
+	// write response to file
+	FILE *fp = fopen("output", "w");
+	if (fp == NULL) {
+		perror("fopen");
+		return 1;
+	}
+	fwrite(content, 1, strlen(content), fp);
+	fclose(fp);
 
 	close(sockfd);
 	return 0;
