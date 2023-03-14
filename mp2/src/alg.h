8065fef1a6de03be29966b73b3ab32c0fb13266a
bekaboo <18127878294@qq.com>
2023-03-13 23:51:49 -0500

    Fix: add include guard in alg.h

--- mp2/src/alg.h
+++ mp2/src/alg.h
@@ -1,3 +1,6 @@
+#ifndef __ALG_H__
+#define __ALG_H__
+
 #define min(a,b) ({                                       \
     typeof(a) _a_temp_;                                   \
     typeof(b) _b_temp_;                                   \
@@ -14,3 +17,4 @@
     _a_temp_ = _a_temp_ < _b_temp_ ? _b_temp_ : _a_temp_; \
 })
 
+#endif /* __ALG_H__ */
