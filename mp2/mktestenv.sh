5abe64ad310cab53729a367963289c4e0d6fa4ec
bekaboo <18127878294@qq.com>
2023-03-19 12:08:08 -0500

    Fix: mktestenv should set loss to 1%

--- mp2/mktestenv.sh
+++ mp2/mktestenv.sh
@@ -2,5 +2,5 @@
 # vim: ft=sh ts=4 sw=4 sts=4 et :
 
 sudo tc qdisc del dev enp0s3 root 2>/dev/null
-sudo tc qdisc add dev enp0s3 root handle 1:0 netem delay 20ms loss 5%
+sudo tc qdisc add dev enp0s3 root handle 1:0 netem delay 20ms loss 1%
 sudo tc qdisc add dev enp0s3 parent 1:1 handle 10: tbf rate 20Mbit burst 10mb latency 1ms
