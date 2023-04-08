42b2d9d6f94069c3fdb71cfd7b6f7081ab8d2b41
AllenHeartcore <ziyuanc3@illinois.edu>
2023-04-08 15:01:00 -0500

    Reading graph, basic Dijkstra (calculating dist) now works

--- mp3/src/distvec.cpp
+++ mp3/src/distvec.cpp
@@ -1,19 +1,175 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NNODE 256
+#define MAX_NMSG  1024
+#define MAX_LMSG  128
+#define DISCONNECTED -999
+#define INF 0x7FFFFFFF
+
+
+class Graph {
+
+
+public:
+	int nnode;
+	bool nodes[MAX_NNODE];
+	int adj[MAX_NNODE][MAX_NNODE];
+	int next[MAX_NNODE][MAX_NNODE];
+	int dist[MAX_NNODE][MAX_NNODE];
+
+
+	Graph() {
+		for (int i = 0; i < MAX_NNODE; i++) {
+			nodes[i] = false;
+			for (int j = 0; j < MAX_NNODE; j++) {
+				if (i == j) {
+					adj[i][j] = 0;
+					next[i][j] = i;
+					dist[i][j] = 0;
+				} else {
+					adj[i][j] = DISCONNECTED;
+					next[i][j] = DISCONNECTED;
+					dist[i][j] = INF;
+				}
+			}
+		}
+	}
+
+
+	void add_edge(int src, int dest, int cost) {
+		if (!nodes[src]) {
+			nodes[src] = true;
+			nnode++;
+		}
+		if (!nodes[dest]) {
+			nodes[dest] = true;
+			nnode++;
+		}
+		adj[src][dest] = cost;
+		adj[dest][src] = cost;
+	}
+
+
+	void dijkstra(int root) {
+
+		// memoization init
+		dist[root][root] = 0;
+		bool visited[MAX_NNODE];
+		for (int i = 0; i < MAX_NNODE; i++) {
+			visited[i] = false;
+		}
+
+		for (int i = 0; i < nnode; i++) {
+
+			// find node closest to visited set
+			int min = INF, min_idx;
+			for (int j = 0; j < MAX_NNODE; j++) {
+				if (nodes[j] && !visited[j] && dist[root][j] < min) {
+					min = dist[root][j];
+					min_idx = j;
+				}
+			}
+
+			// visit and relax edges
+			visited[min_idx] = true;
+			for (int j = 0; j < MAX_NNODE; j++) {
+				if (nodes[j] && !visited[j]
+					&& adj[min_idx][j] != DISCONNECTED
+					&& dist[root][min_idx] + adj[min_idx][j] < dist[root][j]) {
+					dist[root][j] = dist[root][min_idx] + adj[min_idx][j];
+					next[root][j] = min_idx;
+				}
+			}
+		}
+	}
+
+
+	void print() {
+		printf("nnode = %d\n", nnode);
+		printf("[adj]\n");
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j++) {
+				if (adj[i][j] == DISCONNECTED)  printf("X ");
+				else printf("%d ", adj[i][j]);
+			}
+			printf("\n");
+		}
+		printf("[next]\n");
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j++) {
+				if (next[i][j] == DISCONNECTED)  printf("X ");
+				else printf("%d ", next[i][j]);
+			}
+			printf("\n");
+		}
+		printf("[dist]\n");
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 16; j++) {
+				if (dist[i][j] == INF)  printf("X ");
+				else printf("%d ", dist[i][j]);
+			}
+			printf("\n");
+		}
+	}
+};
+
+
+typedef struct message_t {
+	int src;
+	int dest;
+	char data[MAX_LMSG];
+} message_t;
 
 
 int main(int argc, char** argv) {
-    //printf("Number of arguments: %d", argc);
-    if (argc != 4) {
-        printf("Usage: ./distvec topofile messagefile changesfile\n");
-        return -1;
-    }
 
-    FILE *fpOut;
-    fpOut = fopen("output.txt", "w");
-    fclose(fpOut);
+	if (argc != 4) {
+		printf("Usage: ./distvec topofile message_tfile changesfile\n");
+		return -1;
+	}
 
-    return 0;
+	Graph graph;
+	FILE *fpt, *fpm, *fpc, *fpo;
+	char buf[MAX_LMSG], hdata[MAX_LMSG];
+
+	fpt = fopen(argv[1], "r");
+	while (1) {
+		if (!fgets(buf, MAX_LMSG, fpt)) break;
+		int src, dest, cost;
+		sscanf(buf, "%d %d %d", &src, &dest, &cost);
+		graph.add_edge(src, dest, cost);
+	}
+
+	graph.dijkstra(1);
+	graph.print();
+
+	int nm = 0;
+	message_t message_ts[MAX_NMSG];
+	fpm = fopen(argv[2], "r");
+	while (1) {
+		if (!fgets(buf, MAX_LMSG, fpm)) break;
+		sscanf(buf, "%d %d %s", &message_ts[nm].src,
+			&message_ts[nm].dest, hdata);
+		char* hptr = strstr(buf, hdata);
+		strcpy(message_ts[nm].data, hptr);
+		nm++;
+	}
+
+	for (int i = 0; i < nm; i++) {
+		printf("%d %d %s",
+			message_ts[i].src,
+			message_ts[i].dest,
+			message_ts[i].data);
+	}
+
+	// fpc = fopen(argv[3], "r");
+
+
+	// fpo = fopen("output.txt", "w");
+	// fclose(fpo);
+
+	return 0;
 }
 
